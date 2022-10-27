4995afa473abc3c69689fe7d309992fb4cdf364e
Michal Sinielewicz <msinielewicz@timoni.io>
2022-10-27 14:09:49 +0200

    nopcommerce

--- nopcommerce/entrypoint.sh
+++ nopcommerce/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 SOURCEDIR=$INIT_DATA_SOURCEDIR
 
@@ -8,38 +8,39 @@ ERROR=""
 DESTFULL=""
 
 if [ -z "$INIT_DATA_SOURCEDIR" ] ; then
-        ERROR+="Environment variable INIT_DATA_SOURCEDIR not set. "
+        ERROR="Environment variable INIT_DATA_SOURCEDIR not set. "
 fi
 
 if [ -z "$INIT_DATA_DESTDIR" ] ; then
-        ERROR+="Environment variable INIT_DATA_DESTDIR not set. "
+        ERROR="Environment variable INIT_DATA_DESTDIR not set. "
 fi
 
 if [ ! -d $SOURCEDIR ]; then
-        ERROR+="No source directory $SOURCEDIR. "
+        ERROR="No source directory $SOURCEDIR. "
 fi
 
 if [ ! "$(ls -A $SOURCEDIR)" ]; then
-        ERROR+="Source directory $SOURCEDIR was empty. Nothing to copy. "
+        ERROR="Source directory $SOURCEDIR was empty. Nothing to copy. "
 fi
 
 if [ ! -d $DESTDIR ]; then
-        ERROR+="No destination directory $DESTDIR. "
+        ERROR="No destination directory $DESTDIR. "
 fi
 
 if [ "$(ls -A $DESTDIR)" ]; then
-        DESTFULL+="Destination directory $DESTDIR was not empty. "
+        DESTFULL="Destination directory $DESTDIR was not empty. "
 fi
 
 
 if [ -z "$ERROR" ] && [ -z "$DESTFULL" ]; then
-        cp $SOURCEDIR/* $DESTDIR
-        DESTFULL+="Data from $SOURCEDIR was copied to $DESTDIR"
+        cp -r $SOURCEDIR/* $DESTDIR
+        DESTFULL="Data from $SOURCEDIR was copied to $DESTDIR"
         echo $DESTFULL
-else
+fi
 
 if [ -z "$ERROR" ] && [ ! -z "$DESTFULL" ]; then
-        cd /app 
+        cd /app
+        echo 'final function' 
         ln -s /lib/libc.musl-x86_64.so.1 /lib/ld-linux-x86-64.so.2
-        exec dotnet Nop.Web.dll
+         dotnet Nop.Web.dll
 fi
