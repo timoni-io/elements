699c7bc7a2fde5bfce0a5836763a231467042df6
Łukasz Wróblewski <->
2023-10-24 13:45:03 +0200

    redirect

---
+++ ingress/redirect-domain/go.mod
@@ -0,0 +1,3 @@
+module redirect
+
+go 1.20
---
+++ ingress/redirect-domain/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"net/http"
+	"os"
+)
+
+var (
+	targetURL = os.Getenv("TARGET_URL")
+	// targetURL nie może być puste, powinien zawierać protokoł, np http://test.pl
+
+	targetPath = os.Getenv("TARGET_PATH")
+	// w przypadku gdy targetPath jest puste, nastąpi mapowanie ścieżek zapytania na cel jeden do jednego,
+
+	// gdy targetPath = '' & targetURL = 'https://target-domain.pl'
+	// zapytanie = 'https://test.pl/jakis/url'
+	// wynik = 'https://target-domain.pl/jakis/url'
+
+	// gdy targetPath = '/abc' & targetURL = 'https://target-domain.pl'
+	// zapytanie = 'https://test.pl/jakis/url'
+	// wynik = 'https://target-domain.pl/abc'
+
+)
+
+func main() {
+	router := http.NewServeMux()
+	router.HandleFunc("/", redirectHandler)
+	http.ListenAndServe(":80", router)
+}
+
+func redirectHandler(w http.ResponseWriter, r *http.Request) {
+	if targetPath == "" {
+		http.Redirect(w, r, targetURL+r.URL.RawPath, http.StatusTemporaryRedirect)
+
+	} else {
+		http.Redirect(w, r, targetURL+targetPath, http.StatusTemporaryRedirect)
+	}
+}
